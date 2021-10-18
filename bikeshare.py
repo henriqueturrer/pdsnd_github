89308c229aad0b8f11c8d06ae790ddfea5c9feb8
henrique turrer <henrique.turrer@angloamerican.com>
2021-10-18 14:15:08 -0300

    changing restarting message

--- bikeshare.py
+++ bikeshare.py
@@ -263,7 +263,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to do again? Please select yes or no.\n')
         if restart.lower() != 'yes':
             break
 
